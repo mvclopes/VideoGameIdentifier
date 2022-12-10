db691283363ceadbaca014b522f6b403b63314a5
Matheus Lopes <matheus.lopes23@hotmail.com>
2022-12-10 16:44:20 -0300

    Initial Commit

---
+++ MyConsoleClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyConsoleClassifier
+//
+//  Created by Usuário Convidado on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
